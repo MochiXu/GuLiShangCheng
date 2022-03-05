18b1e76c4ae8b4468bcf68ff1b6c1373e5a76829
MochiXu <mirage_mc@icloud.com>
2022-03-05 16:59:49 +0800

    新增nacos配置中心，更换版本(SpringBoot2.2.5.RELEASE,SpringCloud Hoxton.SR3,Alibaba2.2.1.RELEASE)，解决@RefreshScope不生效，解决响应乱码

--- coupon/src/main/java/com/gulimarket/coupon/CouponApplication.java
+++ coupon/src/main/java/com/gulimarket/coupon/CouponApplication.java
@@ -4,6 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 使用nacos作为配置中心，统一管理配置文件
+ * 1.引入依赖
+ * 2.创建一个bootstrap.properties（应用启动的时候首先会加载）
+ * 3.给nacos配置中心默认添加一个数据集Data Id默认规则：应用名.properties
+ * 4.在nacos配置中心使用应用名.properties进行配置
+ * 5.动态获取配置
+ *  @RefreshScope:动态获取并刷新配置
+ *  @Value("${配置项名}"):获取到配置
+ * */
+
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class CouponApplication {
--- coupon/src/main/java/com/gulimarket/coupon/controller/CouponController.java
+++ coupon/src/main/java/com/gulimarket/coupon/controller/CouponController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +18,6 @@ import com.common.utils.PageUtils;
 import com.common.utils.R;
 
 
-
 /**
  * 优惠券信息
  *
@@ -24,25 +25,38 @@ import com.common.utils.R;
  * @email xmc1202@gmail.com
  * @date 2022-03-05 08:00:12
  */
+//此注解用于动态获取并刷新配置文件（配合nacos）,只要是需要使用nacos上面管理的配置文件，都需要用到这个注解
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
 
+    //用于测试nacos配置文件管理
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private Integer age;
+    @RequestMapping("/properties")
+    public R nacosProperties(){
+        System.out.println(name+age);
+        return R.ok().put("name",name).put("age",age);
+    }
+
     //用于测试服务之间的相互调用
     @RequestMapping("/member/list")
-    public R memberCoupons(){
-        CouponEntity couponEntity=new CouponEntity();
+    public R memberCoupons() {
+        CouponEntity couponEntity = new CouponEntity();
         couponEntity.setCouponName("满100减100");
-        return R.ok().put("coupons",Arrays.asList(couponEntity));
+        return R.ok().put("coupons", Arrays.asList(couponEntity));
     }
 
     /**
      * 列表
      */
     @RequestMapping("/list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = couponService.queryPage(params);
 
         return R.ok().put("page", page);
@@ -53,8 +67,8 @@ public class CouponController {
      * 信息
      */
     @RequestMapping("/info/{id}")
-    public R info(@PathVariable("id") Long id){
-		CouponEntity coupon = couponService.getById(id);
+    public R info(@PathVariable("id") Long id) {
+        CouponEntity coupon = couponService.getById(id);
 
         return R.ok().put("coupon", coupon);
     }
@@ -63,8 +77,8 @@ public class CouponController {
      * 保存
      */
     @RequestMapping("/save")
-    public R save(@RequestBody CouponEntity coupon){
-		couponService.save(coupon);
+    public R save(@RequestBody CouponEntity coupon) {
+        couponService.save(coupon);
 
         return R.ok();
     }
@@ -73,8 +87,8 @@ public class CouponController {
      * 修改
      */
     @RequestMapping("/update")
-    public R update(@RequestBody CouponEntity coupon){
-		couponService.updateById(coupon);
+    public R update(@RequestBody CouponEntity coupon) {
+        couponService.updateById(coupon);
 
         return R.ok();
     }
@@ -83,8 +97,8 @@ public class CouponController {
      * 删除
      */
     @RequestMapping("/delete")
-    public R delete(@RequestBody Long[] ids){
-		couponService.removeByIds(Arrays.asList(ids));
+    public R delete(@RequestBody Long[] ids) {
+        couponService.removeByIds(Arrays.asList(ids));
 
         return R.ok();
     }
